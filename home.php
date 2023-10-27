5350bed64eac11f6d4b4896f3e657b42e3d96eb6
Afif Pristantio-A11.2020.12993 <afifpristantio@gmail.com>
2023-10-27 20:10:09 +0700

    revisi 3

--- home.php
+++ home.php
@@ -36,6 +36,19 @@
         </div>
     </nav>
 
+    <main role="main" class="container">
+        <?php 
+        if (isset($_GET['page'])){ ?>
+            <h2><?php echo ucwords($_GET['page']) ?></h2>
+        <?php
+            include($_GET['page'] . ".php");
+        } else { ?>
+            <h3 class="text-center mt-5">Selamat Datang di Sistem Informasi Poliklinik</h3>
+        <?php
+        }
+        ?>
+    </main>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="js/main.js"></script>
 </body>
